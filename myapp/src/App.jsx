acd323ac5417bce63b7c7f651207f38536104722
Satish <davilgaming084@gmail.com>
2024-10-09 01:35:53 +0530

    rect-app-creation

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,34 +1,19 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  let [username, setusername] = useState('')
+  let HandleForm = (e) => {
+    e.preventDefault()
+    console.log(username);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <form onSubmit={(e) => { HandleForm(e) }}>
+        <input value={username} onChange={(e)=>{setusername(e.target.value)}} type="text" className='bg-white p-2 m-4 text-black' />
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
 
