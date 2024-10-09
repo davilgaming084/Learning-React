8dd23e2b0264182992d936f9fe3d732eac9bde92
Satish <davilgaming084@gmail.com>
2024-10-10 01:42:39 +0530

    click-btn-bg-change

--- click-btn-bg-change/src/App.jsx
+++ click-btn-bg-change/src/App.jsx
@@ -1,33 +1,28 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  let [color, setcolor] = useState("")
+  let change = (e) => {
+    console.log(e.target.value);
+    setcolor(e.target.value)
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='h-screen ' style={{backgroundColor: color}}>
+
+        <h1 className='text-red-500'>hi</h1>
+        <div className='absolute inset-x-0 bottom-0'>
+          <button onClick={change} className=" m-2 border-1 rounded-full pl-10 pr-10  bg-red-600 text-white" id="red" value="red">Red</button>
+          <button onClick={change} className=" m-2 border-1 rounded-full pl-10 pr-10 bg-blue-700 text-white" id="blue" value="blue">Blue</button>
+          <button onClick={change} className=" m-2 border-1 rounded-full pl-10 pr-10 bg-green-700 text-white" id="green" value="green">Green</button>
+          <button onClick={change} className=" m-2 border-1 rounded-full pl-10 pr-10 bg-yellow-700 text-white" id="yellow" value="yellow">Yellow</button>
+          <button onClick={change} className=" m-2 border-1 rounded-full pl-10 pr-10 bg-orange-700 text-white" id="orange" value="orange">Orange</button>
+          <button onClick={change} className=" m-2 border-1 rounded-full pl-10 pr-10 bg-purple-700 text-white" id="purple" value="purple">Purple</button>
+          <button onClick={change} className=" m-2 border-1 rounded-full pl-10 pr-10 bg-black text-white" id="black" value="black">Black</button>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
