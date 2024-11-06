674366139bf5a14b54c7a3819bcd1be582fd5209
davilgaming084 <davilgaming084@gmail.com>
2024-11-07 01:46:52 +0530

    delete 12 mega project

--- 12_Mega_Project_Appwrite/src/Components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Logo = ({width = "100px"}) => {
-  return (
-    <div>Logo</div>
-  )
-}
-
-export default Logo
--- 12_Mega_Project_Appwrite/src/Store/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import authReducer from "../Store/authslice"
-const Store = configureStore(
-    {
-        reducer: authReducer
-    }
-)
-
-export default Store
